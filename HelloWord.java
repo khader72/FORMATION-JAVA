f11d2105e87cc3abe20554b48c23372bc470816a
khader72 <124040012+khader72@users.noreply.github.com>
2023-10-11 11:55:01 +0000

    Create HelloWord.java

---
+++ HelloWord.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+//Definir le nom de la class HelloWorld et la rendre public
+public class HelloWorld {//Debut class
+    //Definir le nom de la methode principal et la rendre public
+    public static void main(String[] args) {//Debut methode
+        //Afficher un message  AKWABA, World!
+        System.out.println("AKWABA, World!");
+    }//Fin methode
+    
+    //Definr la methode akwaba
+    public void akwaba(){
+        System.out.println("Bienvenue à Yamoussoukro !");
+    }
+    
+}//Fin class
